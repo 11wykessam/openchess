bf306e2bea1a458bc962a5917276b9f05a332592
Samuel Wykes <samuelwykes@live.co.uk>
2021-07-05 19:05:53 +0100

    First Commit

---
+++ src/main/java/com/whyx/openchess/OpenchessApplication.java
@@ -0,0 +1,13 @@
+package com.whyx.openchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenchessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenchessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whyx/openchess/OpenchessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whyx.openchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenchessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
