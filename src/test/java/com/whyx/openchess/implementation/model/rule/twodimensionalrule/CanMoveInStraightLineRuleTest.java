9d7badacbec55a9f5fd8b0fb8ee8f0a53152c51a
Samuel Wykes <samuelwykes@live.co.uk>
2021-08-16 10:51:11 +0100

    - wrote test to check when move not in straight line.

--- src/test/java/com/whyx/openchess/implementation/model/rule/twodimensionalrule/CanMoveInStraightLineRuleTest.java
+++ src/test/java/com/whyx/openchess/implementation/model/rule/twodimensionalrule/CanMoveInStraightLineRuleTest.java
@@ -76,6 +76,28 @@ class CanMoveInStraightLineRuleTest {
 
             assertThat(rule.moveConformsToRule(move, piece, board)).isTrue();
         }
+
+        @Test
+        void cannotMoveIfNotInStraightLineTest(
+                @Mock final IMove<TwoDimensionalLocation> move,
+                @Mock final ICell<TwoDimensionalLocation> startCell,
+                @Mock final ICell<TwoDimensionalLocation> destinationCell,
+                @Mock final TwoDimensionalLocation startLocation,
+                @Mock final TwoDimensionalLocation destinationLocation,
+                @Mock final IPiece<TwoDimensionalLocation> piece,
+                @Mock final IBoard<TwoDimensionalLocation> board
+        ) {
+            given(move.getStart()).willReturn(startCell);
+            given(move.getDestination()).willReturn(destinationCell);
+            given(startCell.getLocation()).willReturn(startLocation);
+            given(destinationCell.getLocation()).willReturn(destinationLocation);
+            given(startLocation.getX()).willReturn(DEFAULT_X);
+            given(destinationLocation.getX()).willReturn(DEFAULT_X + 1);
+            given(startLocation.getY()).willReturn(DEFAULT_Y);
+            given(destinationLocation.getY()).willReturn(DEFAULT_Y + 1);
+
+            assertThat(rule.moveConformsToRule(move, piece, board)).isFalse();
+        }
     }
 
 }
